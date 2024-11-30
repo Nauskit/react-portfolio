469aedffdd6ccc4e64a184d6629cee2f86d88e85
Nauskit <nutthakit2541@hotmail.com>
2024-11-30 20:09:19 +0700

    Change-photo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ function Hero() {
                             <TypeAnimation
                                 sequence={[
                                     // Same substring at the start will only be typed out once, initially
-                                    'Developer',
+                                    'Full stack Developer',
                                     1000, // wait 1s before replacing "Mice" with "Hamsters"
                                     'Web Developer',
                                     1000,
